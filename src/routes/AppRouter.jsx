2a79e30d4136c059dc85805180bddadc07045e85
Naronglit2015 <nongmnakoy1123@gmail.com>
2024-02-24 16:13:38 +0700

    first commit

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import LoginForm from '../layout/LoginForm';
+import RegisterForm from '../layout/RegisterForm';
+import useAuth from '../hooks/useAuth';
+import Header from '../layout/Header';
+import UserHome from '../layout/UserHome';
+import ProductPage from '../components/ProductPage '; 
+import TableReservations from '../components/TableReservations';
+import Confirm from '../components/Confrim'; 
+import Test001 from '../components/test001';
+
+const guestRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <>
+      <Header />
+      <Outlet />
+    </>,
+    children: [
+      { index: true, element: <LoginForm /> },
+      { path: '/register', element: <RegisterForm /> }
+    ]
+  }
+]);
+
+const userRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <>
+      <Header />
+      <Outlet />
+    </>,
+    children: [
+      { index: true, element: <UserHome /> },
+      { path: '/new', element: <p>New Todo Form</p> },
+      { path: '/products', element: <ProductPage /> },
+      { path: '/table-reservations', element: <TableReservations /> },
+      { path: '/confirm', element: <Confirm /> },
+      { path: '/test001', element: <Test001 /> },
+
+    ]
+  }
+]);
+
+export default function AppRouter() {
+  const { user } = useAuth();
+  const finalRouter = user?.id ? userRouter : guestRouter;
+  return (
+    <RouterProvider router={finalRouter} />
+  );
+}
